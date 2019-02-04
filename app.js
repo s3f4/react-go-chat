ee7203db130aea6ac3b9f04fc1f373363bb34610
s3f4 <sefa@dehaa.com>
2019-02-04 15:36:41 +0300

    Add Channel to ChannelForm

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-let channels = [
-    { "name": "Hardware Support" },
-    { "name": "Software Support" }
-]
-
 class Channel extends React.Component {
     onClick() {
         console.log("i was clicked", this.props.name);
@@ -24,13 +19,13 @@ class ChannelForm extends React.Component {
     }
     onSubmit(e) {
         let { channelName } = this.state;
-        console.log(chanelName);
-        channels.push({
-            "name": channelName
-        });
+        console.log(channelName);
+
         this.setState({
             "channelName": ""
         });
+
+        this.props.addChannel(channelName);
         e.preventDefault();
     }
     render() {
@@ -53,10 +48,28 @@ class ChannelList extends React.Component {
 }
 
 class ChannelSection extends React.Component {
+
+    addChannel(name) {
+        let { channels } = this.state;
+        channels.push({ "name": name });
+        this.setState({
+            "channels": channels
+        });
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            "channels": [
+                { "name": "Hardware Support" },
+                { "name": "Software Support" }
+            ]
+        }
+    }
     render() {
         return <div>
-            <ChannelList channels={channels} />
-            <ChannelForm />
+            <ChannelList channels={this.state.channels} />
+            <ChannelForm addChannel={this.addChannel.bind(this)} />
         </div>
     }
 }
