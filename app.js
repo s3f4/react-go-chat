36809606ec92c057eaada687e91ba7d8f9bd33ac
s3f4 <sefa@dehaa.com>
2019-02-04 15:26:47 +0300

    add channels to array

--- app.js
+++ app.js
@@ -13,9 +13,29 @@ class Channel extends React.Component {
 }
 
 class ChannelForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    onChange(e) {
+        this.setState({
+            channelName: e.target.value
+        });
+    }
+    onSubmit(e) {
+        let { channelName } = this.state;
+        console.log(chanelName);
+        channels.push({
+            "name": channelName
+        });
+        this.setState({
+            "channelName": ""
+        });
+        e.preventDefault();
+    }
     render() {
-        return <form>
-            <input type="text" />
+        return <form onSubmit={this.onSubmit.bind(this)}>
+            <input type="text" onChange={this.onChange.bind(this)} value={this.state.channelName} />
         </form>
     }
 }
