e16f803fbe1a1b64ba1749b637f41dc1cadddadd
s3f4 <sefa@dehaa.com>
2019-02-05 16:08:39 +0300

    last changes

--- rtsupport/components/channels/ChannelSection.jsx
+++ rtsupport/components/channels/ChannelSection.jsx
@@ -7,18 +7,24 @@ import PropTypes from 'prop-types';
 class ChannelSection extends Component {
     render() {
         return (
-            <div>
-                <ChannelList {...this.props} />
-                <ChannelForm {...this.props} />
+            <div className="support panel panel-primary">
+                <div className="panel-heading">
+                    <strong>Channels</strong>
+                </div>
+                <div className="panel-body channels">
+                    <ChannelList {...this.props} />
+                    <ChannelForm {...this.props} />
+                </div>
             </div>
-        );
+        )
     }
 }
 
 ChannelSection.propTypes = {
     channels: PropTypes.array.isRequired,
     setChannel: PropTypes.func.isRequired,
-    addChannel: PropTypes.func.isRequired
+    addChannel: PropTypes.func.isRequired,
+    activeChannel: PropTypes.object.isRequired
 }
 
 export default ChannelSection
